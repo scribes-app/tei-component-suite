4fac7228084832fd22ee4f919ee6bbf3307c4b02
Paul Weinsberg <p.weinsberg@icloud.com>
2024-06-24 15:55:51 +0200

    Scaffold basics requirements
    - Use Sass
    - Use Quill
    - Solve shadow dom exception with Quill.js

--- src/components/xec-editor/xec-editor.tsx
+++ src/components/xec-editor/xec-editor.tsx
@@ -1,17 +1,52 @@
 import { Component, Host, h } from '@stencil/core';
+import Quill from 'quill';
 
 @Component({
   tag: 'xec-editor',
-  styleUrl: 'xec-editor.scss',
-  shadow: true,
+  styleUrls: [
+    'xec-editor.scss'
+  ],
+  /**
+   * We cannot scope the styles because Quill.js uses classes that do not exist before being rendered
+   * The scope will be done using classic CSS selectors
+   * Should be removed when Quill.js will be compatible with shadow DOM
+   */
+  scoped: false,
+  /**
+   * We disable the shadow DOM because Quill.js does not work with it
+   * @see https://github.com/slab/quill/issues/2021
+   */
+  shadow: false,
 })
 export class XecEditor {
 
+  private quillInstance?: Quill;
+  private editorElement?: HTMLDivElement;
+
+  /**
+   * Lifecycle hook called when the component has rendered
+   */
+  public componentDidLoad(): void {
+    this.initQuill();
+  }
+
+  /**
+   * Initialize the Quill editor
+   */
+  private initQuill(): void {
+    this.quillInstance = new Quill(this.editorElement);
+  }
+
+  /**
+   * Render the component
+   */
   render() {
     return (
       <Host>
-        <span>it works</span>
-        <slot></slot>
+        <div
+          class="xec-editor"
+          ref={el => this.editorElement = el}
+        />
       </Host>
     );
   }
--- stencil.config.ts
+++ stencil.config.ts
@@ -2,11 +2,17 @@ import { angularOutputTarget } from '@stencil/angular-output-target';
 import { Config } from '@stencil/core';
 import { reactOutputTarget } from '@stencil/react-output-target';
 import { sass } from '@stencil/sass';
+import { resolve } from 'path';
 
 export const config: Config = {
   namespace: 'xml-editor-cmp',
+  globalStyle: 'src/global.scss',
   plugins: [
-    sass()
+    sass({
+      includePaths: [
+        resolve(__dirname, 'node_modules')
+      ]
+    })
   ],
   outputTargets: [
     angularOutputTarget({
